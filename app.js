bdb6656c4137b70ecc8d8a112361450768010924
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-07 16:41:08 -0600

    bleh

---
+++ app.js
@@ -0,0 +1,14 @@
+var totalRed = 0;
+var totalYellow = 0;
+var totalGreen = 3;
+var totalBlue = 4;
+
+$(document).ready(function() {
+  event.preventDefault();
+
+$(".color-button").data(red).on('click', function() {
+  $(".container").append("<div class='red'></div>")
+});
+
+
+});
