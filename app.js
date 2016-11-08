f8e707f789e5d0ec96caed13a15f0a6fa676bc15
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-07 20:54:07 -0600

    functional game

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-var totalRed = 0;
-var totalYellow = 0;
-var totalGreen = 3;
-var totalBlue = 4;
+var redTotal = 0;
+var yellowTotal = 0;
+var greenTotal = 0;
+var blueTotal = 0;
+var total = 0;
 
 $(document).ready(function() {
   event.preventDefault();
 
-$(".color-button").data(red).on('click', function() {
-  $(".container").append("<div class='red'></div>")
-});
+  $(".color-button").click(function() {
+    console.log("pls add");
+    colorDecider($(this).data('color'));
+    console.log();
+  });
 
-
+  //counts colors each time the respective button is pressed
+  function colorDecider(color) {
+    if(color == "red") {
+      redTotal++;
+      $("#red").text("Total red: " + redTotal);
+    } else if (color == "yellow") {
+      yellowTotal++;
+      $("#yellow").text("Total yellow: " + yellowTotal);
+    } else if (color == "green") {
+      greenTotal++;
+      $("#green").text("Total green: " + greenTotal);
+    } else if (color == "blue") {
+      blueTotal++;
+      $("#blue").text("Total blue: " + blueTotal);
+    }
+    $(".container").append('<div class= "color-cube ' + color + '"></div>'); 
+  }
 });
